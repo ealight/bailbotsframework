afb654bf8d8b82bf7da1bc157f2e6702bfcfb33e
dburk <ealightpawn@gmail.com>
2021-12-09 19:19:09 +0200

    updated data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,4 @@ INSERT INTO stickers (id, url)
 VALUES (1, 'https://blog.jiayu.co/2019/07/telegram-animated-stickers/sticker.webp'),
        (2, 'https://chpic.su/_data/stickers/p/PresidentPutin/PresidentPutin_001.webp'),
        (3, 'https://img-16.stickers.cloud/packs/f02e2ff4-63c9-42d6-9c3d-5c54eb604210/webp/7813c1ec-2384-4e89-a5d5-f18be88d925d.webp')
-ON CONFLICT DO NOTHING/UPDATE;
+ON CONFLICT DO NOTHING;
