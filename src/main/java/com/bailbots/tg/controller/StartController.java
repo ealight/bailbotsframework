43f5079ba8b512e2d28c655565dc643486ebb092
dburk <ealightpawn@gmail.com>
2021-12-09 14:25:55 +0200

    added commands for stickers

--- src/main/java/com/bailbots/tg/controller/StartController.java
+++ src/main/java/com/bailbots/tg/controller/StartController.java
@@ -3,25 +3,33 @@ package com.bailbots.tg.controller;
 import com.bailbots.tg.bpp.annotation.BotController;
 import com.bailbots.tg.bpp.annotation.BotRequestMapping;
 import com.bailbots.tg.model.Event;
+import com.bailbots.tg.model.Sticker;
+import com.bailbots.tg.repository.StickerRepository;
 import com.bailbots.tg.service.EventsService;
 import com.bailbots.tg.service.KeyboardLoaderService;
 import com.bailbots.tg.service.MessageService;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
-import java.util.Set;
 
 @BotController
 public class StartController {
+    private static final String ADD_STICKER_COMMAND = "/addsticker";
+    private static final String DELETE_STICKER_COMMAND = "/removesticker";
+    private static final String WEEKEND_COMMAND = "/weekend";
+
     private final MessageService messageService;
     private final KeyboardLoaderService keyboardLoaderService;
     private final EventsService eventsService;
+    private final StickerRepository stickerRepository;
 
-    public StartController(MessageService messageService, KeyboardLoaderService keyboardLoaderService, EventsService eventsService) {
+    public StartController(MessageService messageService, KeyboardLoaderService keyboardLoaderService, EventsService eventsService, StickerRepository stickerRepository) {
         this.messageService = messageService;
         this.keyboardLoaderService = keyboardLoaderService;
         this.eventsService = eventsService;
+        this.stickerRepository = stickerRepository;
     }
 
 
@@ -37,12 +45,12 @@ public class StartController {
                 keyboardLoaderService.getStaticKeyboardFromXML("start"), telegramId);
     }
 
-    @BotRequestMapping("/addEvent")
+    @BotRequestMapping("/add")
     public void choseHouse(Update update) {
         eventsService.addEvent(update);
     }
 
-    @BotRequestMapping("/allEvents")
+    @BotRequestMapping("/all")
     public void allEvents(Update update) {
         showEvents(update, eventsService.getAllEvents());
     }
@@ -52,7 +60,7 @@ public class StartController {
         showEvents(update, eventsService.getAllEvents());
     }
 
-    @BotRequestMapping("/todayEvents")
+    @BotRequestMapping("/today")
     public void today(Update update) {
         showEvents(update, eventsService.getAllEventsToday());
     }
@@ -68,25 +76,57 @@ public class StartController {
         messageService.sendMessage("Введіть: /addEvent [Назва] [Час, пр: 14:00] [Дата, пр: 22.09.2005]", chatId);
     }
 
-    @BotRequestMapping("/weekend")
+    @BotRequestMapping(WEEKEND_COMMAND)
     public void weekend(Update update) {
         Long chatId = update.getMessage().getChatId();
         String text = update.getMessage().getText();
-        String time = text.substring(text.indexOf("/weekend") + 8);
+        String time = text.substring(text.indexOf(WEEKEND_COMMAND) + WEEKEND_COMMAND.length());
 
-        eventsService.setWeekendTime(time);
+        if (time.isEmpty()) {
+            eventsService.getWeekendTime();
+        } else {
+            eventsService.setWeekendTime(time);
+        }
 
         messageService.sendMessage(String.format("Час засетаний на %s", time), chatId);
+    }
+
+    @BotRequestMapping(ADD_STICKER_COMMAND)
+    public void addSticker(Update update) {
+        Long chatId = update.getMessage().getChatId();
+        String text = update.getMessage().getText();
+        String url = text.substring(text.indexOf(ADD_STICKER_COMMAND) + ADD_STICKER_COMMAND.length());
+
+        Sticker sticker = Sticker.builder()
+                .url(url)
+                .build();
+
+        stickerRepository.save(sticker);
+
+        messageService.sendMessage(String.format(
+                "Новий стікер додано: ID: %s | URL: %s", sticker.getId(), sticker.getUrl()
+        ), chatId);
+    }
+
+    @BotRequestMapping(DELETE_STICKER_COMMAND)
+    public void removeSticker(Update update) {
+        Long chatId = update.getMessage().getChatId();
+        String text = update.getMessage().getText();
+        String id = text.substring(text.indexOf(DELETE_STICKER_COMMAND) + DELETE_STICKER_COMMAND.length());
+
+        Optional<Sticker> sticker = stickerRepository.findById(Long.valueOf(id));
+
+        sticker.ifPresentOrElse(stickerRepository::delete, () -> messageService.sendMessage(
+                String.format("Стікера з ID %s не існує", id), chatId));
     }
 
     private void showEvents(Update update, List<Event> events) {
         Long chatId = update.getMessage().getChatId();
         StringBuilder builder = new StringBuilder();
 
-        if(events.isEmpty()) {
+        if (events.isEmpty()) {
             builder.append("Немає :(");
-        }
-        else {
+        } else {
             events
                     .forEach(event -> builder.append(event.toString()));
         }
